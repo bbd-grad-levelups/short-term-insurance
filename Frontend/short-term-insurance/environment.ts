195c42cf95d02f7b322ae21642ace6f67fd45602
KarlEtsebeth <148228664+KarlEtsebeth@users.noreply.github.com>
2024-06-27 22:26:28 +0200

    Test trailing /

--- Frontend/short-term-insurance/environment.ts
+++ Frontend/short-term-insurance/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'http://localhost:4200',
-  authorizationUrl: 'https://short-term-insurance.auth.eu-west-1.amazoncognito.com/oauth2/authorize?client_id=3bg09ucl23vla92ug243bnmqft&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Finsurance.projects.bbdgrad.com'
+  authorizationUrl: 'https://short-term-insurance.auth.eu-west-1.amazoncognito.com/oauth2/authorize?client_id=3bg09ucl23vla92ug243bnmqft&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Finsurance.projects.bbdgrad.com/'
 };
