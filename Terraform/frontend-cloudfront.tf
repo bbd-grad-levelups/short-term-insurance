08911339bb25c73d52707b496321bf2800cf0fa3
KarlEtsebeth <148228664+KarlEtsebeth@users.noreply.github.com>
2024-06-27 22:15:39 +0200

    Temp disable security headers

--- Terraform/frontend-cloudfront.tf
+++ Terraform/frontend-cloudfront.tf
@@ -21,32 +21,32 @@ resource "aws_cloudfront_response_headers_policy" "frontend" {
   name    = "security-policy"
 
   security_headers_config {
-    content_type_options {
-      override = true
-    }
     frame_options {
       frame_option = "DENY"
       override = true
     }
-    referrer_policy {
-      referrer_policy = "same-origin"
-      override = true
-    }
-    xss_protection {
-      mode_block = true
-      protection = true
-      override = true
-    }
-    strict_transport_security {
-      access_control_max_age_sec = "63072000"
-      include_subdomains = true
-      preload = true
-      override = true
-    }
-    content_security_policy {
-      content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; font-src 'self' https://fonts.gstatic.com/ https://fonts.googleapis.com/; connect-src 'self' http://localhost:5000 https://fonts.googleapis.com/ https://fonts.gstatic.com/;"
-      override = true
-    }
+    # content_type_options {
+    #   override = true
+    # }
+    # referrer_policy {
+    #   referrer_policy = "same-origin"
+    #   override = true
+    # }
+    # xss_protection {
+    #   mode_block = true
+    #   protection = true
+    #   override = true
+    # }
+    # strict_transport_security {
+    #   access_control_max_age_sec = "63072000"
+    #   include_subdomains = true
+    #   preload = true
+    #   override = true
+    # }
+    # content_security_policy {
+    #   content_security_policy = "frame-ancestors 'none'; default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; font-src 'self' https://fonts.gstatic.com/ https://fonts.googleapis.com/; connect-src 'self' http://localhost:5000 https://fonts.googleapis.com/ https://fonts.gstatic.com/ https://api.insurance.projects.bbdgrad.com/;"
+    #   override = true
+    # }
   }
 }
 
