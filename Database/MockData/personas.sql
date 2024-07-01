ca169d9829ea97e61a8deccf5079e0e80aea7c5d
Johan Viljoen <johan.viljoen@bbd.co.za>
2024-07-01 18:39:31 +0200

    Update personas.sql

--- Database/MockData/personas.sql
+++ Database/MockData/personas.sql
@@ -1,5 +1,5 @@
 --changeset johan:dml:mockData:personas
 INSERT INTO personas (persona_id, electronics, last_payment_date, debit_order_id) VALUES
-(1, 3, "01|01|01", 123312),
-(2, 1, "01|01|01", 1289582);
+(1, 3, '01|01|01', 123312),
+(2, 1, '01|01|01', 1289582);
 --rollback DELETE FROM "personas";
