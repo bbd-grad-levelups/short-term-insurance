85bc21200d2c318218c725ffa5daa847ead34e25
Ivan Cuyler <Ivan.Cuyler@bbd.co.za>
2024-07-02 18:45:47 +0200

    Added date filter to Logs page

--- Frontend/short-term-insurance/src/pages/logs-page/logs-page.component.ts
+++ Frontend/short-term-insurance/src/pages/logs-page/logs-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild, ChangeDetectionStrategy, Type } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from "@angular/material/table";
 import { MatPaginator, MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -9,7 +9,13 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Logs } from '../../models/logs.model';
 import { MatButtonModule } from '@angular/material/button';
 import { InsuranceService } from '../../services/insurance.service';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
+import {JsonPipe} from '@angular/common';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { L } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-logs-page',
@@ -19,14 +25,28 @@ import { InsuranceService } from '../../services/insurance.service';
     MatPaginatorModule,
     MatIcon,
     MatProgressSpinnerModule,
-    MatTableModule],
+    MatTableModule,
+    MatFormFieldModule, 
+    MatDatepickerModule,
+    MatFormFieldModule, 
+    MatDatepickerModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    JsonPipe],
+
   templateUrl: './logs-page.component.html',
-  styleUrl: './logs-page.component.css'
+  styleUrl: './logs-page.component.css',
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LogsPageComponent {
   @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator(
     new MatPaginatorIntl(), ChangeDetectorRef.prototype
   );
+  readonly range = new FormGroup({
+    start: new FormControl<Date>(new Date()),
+    end: new FormControl<Date>(new Date()),
+  });
 
   dataSource: MatTableDataSource<Logs> = new MatTableDataSource<Logs>();
 
@@ -35,23 +55,44 @@ export class LogsPageComponent {
   loading: boolean = true;
   isLastPage: boolean = false;
   page: number = 1;
+  endDate: Date | null = null;
+  beginDate: Date | null = null;
 
   constructor(
     private insuranceService: InsuranceService,
     private snackBar: MatSnackBar
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.getLogsData();
   }
+   
+  
+  public filterButtonClicked(){
+    this.beginDate = this.range.controls.start.value;
+    this.endDate = this.range.controls.end.value;
+
+    this.getLogsData();
+   }
+
 
   getLogsData(nextPage: boolean = false) {
-    const endDate = new Date();
-    const beginDate = new Date(0);
+    if (this.range.controls.start.value){
+      this.beginDate = this.range.controls.start.value;
+    }
+    else {
+      this.beginDate = new Date(0);
+    }
+    if (this.range.controls.end.value){
+      this.endDate = this.range.controls.end.value;
+    }
+    else {
+      this.endDate = new Date(0);
+    }
 
     this.error = false;
     this.loading = true;
-    this.insuranceService.getLogs(beginDate, endDate, this.page)
+    this.insuranceService.getLogs(this.beginDate, this.endDate, this.page)
       .subscribe({
         next: response => {
           this.isLastPage = response.page === response.availablePages;
